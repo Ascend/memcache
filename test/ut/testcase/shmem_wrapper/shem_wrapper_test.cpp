52d751e6fff86345cce10a7fa5919f2a67bd1db1
minibao <baoxiaoming@huawei.com>
2025-04-18 17:47:06 +0000

    !25 added wrapper api and test * Merge remote-tracking branch 'origin/newMaster' into newMaster * added wrapper api and test * 1 added some log into hybm_core_api.cpp * 1 refined cmake list of UT * Merge remote-tracking branch 'origin/newMaster' into newMaster * added wrapper api and test * added wrapper api and test * Merge remote-tracking branch 'origin/newMaster' into newMaster * 1 added some log into hybm_core_api.cpp * 1 refined cmake list of UT * Merge remote-tracking branch 'origin/newMaster' into newMaster * 1 added some log into hybm_core_api.cpp * Merge remote-tracking branch 'memfabric_hybrid_offical/master' into newMaster * 1 refined cmake list of UT

---
+++ test/ut/testcase/shmem_wrapper/shem_wrapper_test.cpp
@@ -0,0 +1,38 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025-2026. All rights reserved.
+ */
+#include <gtest/gtest.h>
+
+#include "smem_shm_api.h"
+
+using namespace shm;
+
+class ShmemWrapperTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+    }
+
+    static void TearDownTestCase()
+    {
+    }
+
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+};
+
+TEST_F(ShmemWrapperTest, smem_api_test)
+{
+    std::string outLibPath = OUT_LIB_PATH;
+    ShmOutLogger::Instance().SetLogLevel(shm::LogLevel::DEBUG_LEVEL);
+    ASSERT_EQ(SmemApi::LoadLibrary("") != 0, true);
+    ASSERT_EQ(SmemApi::LoadLibrary(outLibPath + "/smem/lib/") == 0, true);
+
+    // ASSERT_EQ(SmemApi::SmemInit(0) != 0, true);
+}
+
